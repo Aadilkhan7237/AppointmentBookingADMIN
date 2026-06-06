e6b32c1a76846decca9a6baf08acf0984e5170cd
aadiluser2005 <aadiluser2005@gmail.com>
2026-06-06 23:18:49 +0530

    Fixed typo

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -6,110 +6,115 @@ import jwt from "jsonwebtoken";
 
 
 
-export const settingInfo=async(req,res)=>{
+export const settingInfo = async (req, res) => {
 
-    try {
+  try {
 
-        // const {adminID}=req.admin;
-        
-        const admin=await adminModel.findOne({});
-          
-        const infoObj={
-            fullName:admin.fullName,
-            email:admin.email,
-            phoneNumber:admin.phoneNumber
-        }
+    // const {adminID}=req.admin;
 
-        res.status(200).json({message:infoObj});
+    const admin = await adminModel.findOne({});
 
-    } catch (error) {
-        console.log(error);
-         res.status(500).json({message:"Cant fetch info"});
+    const infoObj = {
+      fullName: admin.fullName,
+      email: admin.email,
+      phoneNumber: admin.phoneNumber
     }
+
+    res.status(200).json({ message: infoObj });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Cant fetch info" });
+  }
 }
 
 
-export const register=async(req,res)=>{
-         
-    try {
+export const register = async (req, res) => {
 
-        const {fullName,email,phone,pass}=req.body;
+  try {
 
-        const salt=Number(process.env.SALT_ROUNDS);
+    const { fullName, email, phone, pass } = req.body;
 
-        const hashedPass=await bcrypt.hash(pass,salt);
+    const salt = Number(process.env.SALT_ROUNDS);
 
-        
-        const admin=new adminModel({
-            fullName:fullName,
-            email:email,
-            phoneNumber:phone,
-            password:hashedPass,
-        });
+    const hashedPass = await bcrypt.hash(pass, salt);
 
-        await admin.save();
 
-        res.status(200).json({message:"Register Admin Successfully"});
-        
-    } catch (error) {
-         res.status(500).json({message:"Error while registering"});
-    }
+    const admin = new adminModel({
+      fullName: fullName,
+      email: email,
+      phoneNumber: phone,
+      password: hashedPass,
+    });
+
+    await admin.save();
+
+    res.status(200).json({ message: "Register Admin Successfully" });
+
+  } catch (error) {
+    res.status(500).json({ message: "Error while registering" });
+  }
 }
 
 
-export const login=async(req,res)=>{
-    
-      
-     try {
-         const { email, pass } = req.body;
-        //   console.log(email,"       ",pass);
+export const login = async (req, res) => {
 
-        // return res.status(200).json({message:"Working"});
 
-       const admin = await adminModel.findOne({ email:email });
-   
-      
-   
-       if (!admin) {
-         res
-           .status(500)
-           .json({ message: "Email or password is incorrect" });
-         return;
-       }
+  try {
+    const { email, pass } = req.body;
+    //   console.log(email,"       ",pass);
 
-       const ispasswordMatched = await bcrypt.compare(pass, admin.password);
-   
-       if (ispasswordMatched) {
-         const token = jwt.sign({ adminId: admin._id }, process.env.SECRET, {
-           algorithm: "HS512",
-           expiresIn: "1h",
-         });
+    // return res.status(200).json({message:"Working"});
 
-         //console.log(token);
-         const isProduction = process.env.NODE_ENV === "production";
-         res.cookie("jwtADMIN", token, {
-           httpOnly: true,
-           secure: isProduction,
-           sameSite: isProduction ? "none" : "lax",
-           path: "/",
-         });
-         res
-           .status(200)
-           .json({ message: "Admin logged in successfully" });
-       } else {
-         res
-           .status(500)
-           .json({ message: "Email or password is incorrect" });
-       }
-     } catch (e) {
-       res.status(500).json({ message: `${e}` });
-     }
+    const admin = await adminModel.findOne({ email: email });
+
+
+
+    if (!admin) {
+      res
+        .status(500)
+        .json({ message: "Email or password is incorrect" });
+      return;
+    }
+
+    const ispasswordMatched = await bcrypt.compare(pass, admin.password);
+
+    if (ispasswordMatched) {
+      const token = jwt.sign({ adminId: admin._id }, process.env.SECRET, {
+        algorithm: "HS512",
+        expiresIn: "1h",
+      });
+
+      //console.log(token);
+      const isProduction = process.env.NODE_ENV === "production";
+
+      console.log('NODE_ENV in OAuth:', process.env.NODE_ENV);
+      console.log('isProduction in OAuth :', isProduction);
+      console.log('FRONTEND_URL in OAuth :', process.env.DASHBOARD_URL);
+
+      res.cookie("jwtADMIN", token, {
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction ? "None" : "lax",
+        path: "/",
+      });
+      res
+        .status(200)
+        .json({ message: "Admin logged in successfully" });
+    } else {
+      res
+        .status(500)
+        .json({ message: "Email or password is incorrect" });
+    }
+  } catch (e) {
+    res.status(500).json({ message: `${e}` });
+  }
 }
 
 
 export const logout = async (req, res) => {
   const token = req.cookies.jwtADMIN;
-   console.log(token);
+  console.log(token);
   try {
     if (!token) {
       return res
@@ -122,7 +127,7 @@ export const logout = async (req, res) => {
       res.clearCookie("jwtADMIN", {
         httpOnly: true,
         secure: isProduction,
-        sameSite: isProduction ? "none" : "lax",
+        sameSite: isProduction ? "None" : "lax",
         path: "/",
       });
 
@@ -138,64 +143,64 @@ export const logout = async (req, res) => {
 
 
 
-export const profileChange=async(req,res)=>{
-         
-    try {
+export const profileChange = async (req, res) => {
 
-        const {fullName,email,phone}=req.body;
+  try {
 
-      const admin =await adminModel.findOne({email:email});
+    const { fullName, email, phone } = req.body;
 
-      if(admin.fullName===fullName&&admin.email===email&&admin.phoneNumber===phone){
-        return res.status(201).json({message:"No changes found"});
-      }
+    const admin = await adminModel.findOne({ email: email });
 
-      admin.fullName=fullName;
-      admin.email=email;
-      admin.phoneNumber=phone;
-
-      await admin.save();
-
-        res.status(200).json({message:"Profile Info changed successfully"});
-        
-    } catch (error) {
-         res.status(500).json({message:"Error while changing profile info"});
+    if (admin.fullName === fullName && admin.email === email && admin.phoneNumber === phone) {
+      return res.status(201).json({ message: "No changes found" });
     }
+
+    admin.fullName = fullName;
+    admin.email = email;
+    admin.phoneNumber = phone;
+
+    await admin.save();
+
+    res.status(200).json({ message: "Profile Info changed successfully" });
+
+  } catch (error) {
+    res.status(500).json({ message: "Error while changing profile info" });
+  }
 }
 
 
 
-export const passChanges=async(req,res)=>{
-         
-    try {
-         const {adminId}=req.admin;
-        const {newPass,oldPass}=req.body;
+export const passChanges = async (req, res) => {
 
-        //console.log(oldPass,"  ",newPass);
+  try {
+    const { adminId } = req.admin;
+    const { newPass, oldPass } = req.body;
 
-        
-     
-        const admin=await adminModel.findOne({_id:adminId});
+    //console.log(oldPass,"  ",newPass);
 
-        const isPassswordMatch=await bcrypt.compare(oldPass,admin.password);
 
-        if(isPassswordMatch){
-            
-            const salt = Number(process.env.SALT_ROUNDS);
-            const hashedPass=await bcrypt.hash(newPass,salt);
 
-            admin.password=hashedPass;
+    const admin = await adminModel.findOne({ _id: adminId });
 
-            await admin.save();
+    const isPassswordMatch = await bcrypt.compare(oldPass, admin.password);
 
-            return res.status(200).json({message:"Password changed successfully"});
+    if (isPassswordMatch) {
 
-        }else{
-            return res.status(500).json({message:"Old password is incorrect"});
-        }
+      const salt = Number(process.env.SALT_ROUNDS);
+      const hashedPass = await bcrypt.hash(newPass, salt);
 
-        
-    } catch (error) {
-         res.status(500).json({message:"Error while pass cahnges"});
+      admin.password = hashedPass;
+
+      await admin.save();
+
+      return res.status(200).json({ message: "Password changed successfully" });
+
+    } else {
+      return res.status(500).json({ message: "Old password is incorrect" });
     }
+
+
+  } catch (error) {
+    res.status(500).json({ message: "Error while pass cahnges" });
+  }
 }
